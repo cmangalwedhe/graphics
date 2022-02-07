1839a29b46a729e6712970af7f5cb54241817d2c
Chinmay Mangalwedhe <S791455@cfisd.loc>
2022-02-07 13:01:44 -0600

    added car animation

--- src/Picture.java
+++ src/Picture.java
@@ -12,7 +12,44 @@ public class Picture extends JPanel {
     private static final Color groundColor = new Color(152, 182, 142);
     private static final Color roadColor = new Color(79, 78, 78);
     private static final Color signColor = new Color(77, 101, 72);
+    private int bodyX,  bodyY,  tireOneX,  tireOneY, width, height, tireTwoX, tireTwoY;
 
+    /**
+     * Animates the car to move forward on the highway
+     */
+    public void animateCar() {
+
+        // sets initial values of car's movement
+        bodyX = 450;
+        bodyY = 500;
+        tireOneX = 475;
+        tireOneY = 585;
+        tireTwoX = 600;
+        tireTwoY = 585;
+        width = 200;
+        height = 100;
+
+
+        for (int i = 0; i < 500; i += 10) {
+            // delays movement of the car to show it moving
+            try {
+                Thread.sleep(300);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // changes the position of the car for it to move
+            width -= 1;
+            height -= 1;
+            bodyY -= 14;
+            tireOneX += .1 * width;
+            tireOneY -= 14 / height;
+            tireTwoX -= .1;
+            tireTwoY -= 14;
+            repaint();
+        }
+    }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
@@ -26,26 +63,44 @@ public class Picture extends JPanel {
         drawRoad(g, new int[]{0, 400, 800}, new int[]{600, 0, 600});
         drawRoadDivider(g);
         drawNames(g);
+        drawCar(g);
 
     }
 
+    // that method right there is where I animate it
+    // so what do you recommend?
+    //
+
+    /**
+     * Draws the road as a triangle to show perspective and a little bit o
+     * f 3D art
+     * @param g a reference to the graphics container
+     * @param xPts an array of x coordinates to draw the road
+     * @param yPts an array of y coordinates to draw the road
+     */
     public void drawRoad(Graphics g, int[] xPts, int[] yPts) {
-
         g.setColor(roadColor);
-
 
         Polygon mainRoad = new Polygon(xPts, yPts, 3);
         g.fillPolygon(mainRoad);
     }
 
+    /**
+     * Draws the road divider for the road to distinguish the lanes
+     * @param g a reference to the graphics container
+     */
+
     public void drawRoadDivider(Graphics g) {
         // make road divider color yellow and draw divider
         g.setColor(new Color(177, 169, 50));
         for (int i = 10; i < 585; i+= 30) {
-            g.fillRect(398, i, 5, 13);
+            g.fillRect(398, i, 5, 12);
         }
     }
 
+    /**
+     * @param g a reference to the graphics container
+     */
     public void drawRamp(Graphics g) {
         // draws exit 12b ramp
         g.setColor(roadColor);
@@ -94,15 +149,15 @@ public class Picture extends JPanel {
     public void drawSun(Graphics g) {
         // draw the sun
         g.setColor(new Color(241, 197, 109));
-        g.fillArc(20, -50, 100, 100, 0, -180);
+        g.fillArc(25, -50, 100, 100, 0, -180);
 
         // draw the sun lining
         g.setColor(new Color(255, 129, 0));
-        g.drawArc(20, -50, 100, 100, 0, -180);
+        g.drawArc(25, -50, 100, 100, 0, -180);
     }
 
     public void welcomeSign(Graphics g) {
-        // draws Welcome in English, Kannada, and Japanese
+        // draws Welcome to US sign
         g.setColor(signColor);
         g.fillRect(10, 125, 210, 150);
 
@@ -135,6 +190,24 @@ public class Picture extends JPanel {
         nameWriter.drawString("Braeden Drosche - Period 6", 120, 110);
     }
 
+    /**
+     * Draws the car on one lane of the highway, entering the United States
+     * @param carDrawer reference to graphics container
+     */
+
+
+    public void drawCar(Graphics carDrawer) {
+
+        carDrawer.setColor(new Color(179, 222, 192));
+
+        // draws the car
+        carDrawer.fillRect(bodyX, bodyY, width, height);
+
+        carDrawer.setColor(Color.BLACK);
+        carDrawer.drawOval(tireOneX, tireOneY, 10, 20);
+        carDrawer.drawOval(tireTwoX, tireTwoY, 10, 20);
+    }
+
     public static void main(String[] args) {
         // basic frame configuration
         JFrame frame = new JFrame();
@@ -153,5 +226,7 @@ public class Picture extends JPanel {
 
         // centers the frame
         frame.setLocationRelativeTo(null);
+
+        component.animateCar();
     }
 }
